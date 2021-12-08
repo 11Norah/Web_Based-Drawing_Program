022ee134a73fdc9a334cb6b3aceaa7b7c0d0d9da
Aliaa Fathy <94761307+AliaaFathy@users.noreply.github.com>
2021-12-08 15:11:04 +0200

    Update shapes.component.ts

--- Frontend/src/app/shapes/shapes.component.ts
+++ Frontend/src/app/shapes/shapes.component.ts
@@ -62,6 +62,105 @@ export class SHAPESComponent implements OnInit {
     } 
     
    }
+     circle(){
+      this.isDrawing =false;
+      let x=0;
+      let y=0;
+      let lasty=0;
+      let lastx=0;
+      let radious=0;
+      let m1=0;
+      let m2=0;
+      onmousedown = (event: MouseEvent) => { 
+         x=event.clientX;
+         y=event.clientY;
+         onmousedown = (event: MouseEvent) => { 
+          lastx=event.clientX;
+          lasty=event.clientY; 
+          
+          console.log(x,y);
+          radious=Math.sqrt(Math.pow((lastx-x),2)+Math.pow((lasty-y),2));
+          console.log(radious);
+          this.context.beginPath();
+              this.context.arc(x,y,radious,0,2*Math.PI);
+              this.context.stroke();
+              this.circle();
+              this.context.beginPath();
+     
+    } 
+    }}
+    ellipse(){
+      this.isDrawing =false;
+      let x=0;
+      let y=0;
+      let lasty=0;
+      let lastx=0;
+      let radiousx=0;
+      let radiousy=0;
+      let x2=0;
+      let y2=0;
+      onmousedown = (event: MouseEvent) => { 
+         x=event.clientX;
+         y=event.clientY;
+         onmousedown = (event: MouseEvent) => { 
+          lastx=event.clientX;
+          lasty=event.clientY; 
+          onmousedown=(event:MouseEvent)=>{
+            x2=event.clientX;
+            y2=event.clientY;
+      
+          console.log(x,y);
+          console.log(x2,y2);
+          radiousx=Math.sqrt(Math.pow((lastx-x),2)+Math.pow((lasty-y),2));
+          radiousy=Math.sqrt(Math.pow((x2-x),2)+Math.pow((y2-y),2));
+          console.log(radiousx);
+          console.log(radiousy);
+          this.context.beginPath();
+              this.context.ellipse(x,y,radiousx,radiousy,Math.PI,0,2*Math.PI);
+              this.context.stroke();
+              this.ellipse();
+              this.context.closePath();
+              
+
+    }
+  }
+  }}
+     triangle(){
+    this.isDrawing =false;
+    let x=0;
+    let y=0;
+    let lasty=0;
+    let lastx=0;
+    let radiousx=0;
+    let radiousy=0;
+    let x2=0;
+    let y2=0;
+    onmousedown = (event: MouseEvent) => { 
+       x=event.clientX;
+       y=event.clientY;
+       onmousedown = (event: MouseEvent) => { 
+        lastx=event.clientX;
+        lasty=event.clientY; 
+        onmousedown=(event:MouseEvent)=>{
+          x2=event.clientX;
+          y2=event.clientY;
+    
+        
+        this.context.beginPath();
+            this.context.moveTo(x,y);
+            this.context.lineTo(lastx,lasty);
+            this.context.lineTo(x2,y2);
+            this.context.moveTo(x2,y2);
+            this.context.lineTo(x,y);
+            this.context.stroke();
+            this.triangle();
+            this.context.closePath();
+           
+
+  }
+}
+}
+  }
     drawLine() {
     let prevX=0
     let prevY=0;
@@ -95,8 +194,6 @@ function getCanvasRenderingContext2D(arg0: any): CanvasRenderingContext2D {
 function getCanvasElementById(arg0: string): any {
   throw new Error('Function not implemented.');
 }
-
-
 
 
 
